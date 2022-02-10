3e538ced876965175c7afc64e2405f16d3143404
Lelabeshidze <lela.beshidze@gmail.com>
2022-02-11 01:24:56 +0400

    change images

--- project.js
+++ project.js
@@ -10,6 +10,6 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
   slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 2000); 
+  setTimeout(showSlides, 5000); 
 }
 window.onload = showSlides();
