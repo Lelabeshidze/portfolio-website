30df87c0cd736c49f134750a3a588daa625caaa1
Lelabeshidze <lela.beshidze@gmail.com>
2022-02-19 23:53:39 +0400

    Why is javascript so hard?

--- project.js
+++ project.js
@@ -138,3 +138,76 @@ function createFilterForProjects() {
 
 createFilterForProjects();
 }
+
+
+// Get the modal
+var modal = document.getElementById("user-notification");
+
+var btn = document.querySelector(".sendbutton");
+
+var span = document.getElementsByClassName("modal-close")[0];
+
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+{
+const btn = document.querySelector('.sendbutton');
+
+function sendData() {
+
+  const XHR = new XMLHttpRequest();
+  
+  
+    const name = document.querySelector('#name');
+    const email = document.querySelector('#email');
+    const website = document.querySelector('#website');
+    const message = document.querySelector('#message');
+    
+    const nameVal = name.value;
+    const emailVal = email.value;
+    const websiteVal = website.value;
+    const messageVal = message.value;
+
+  let urlEncodedData = "",
+      urlEncodedDataPairs = [nameVal,emailVal,websiteVal,messageVal];
+
+  // Turn the data object into an array of URL-encoded key/value pairs.
+  for(i = 0;  i++;) {
+    urlEncodedDataPairs.push(  encodeURIComponent(name) +'&email=' + encodeURIComponent(email)  +'&website=' + encodeURIComponent(website) +'&message=' + encodeURIComponent(message)  );
+  }
+
+  // Combine the pairs into a single string and replace all %-encoded spaces to
+  // the '+' character; matches the behavior of browser form submissions.
+  urlEncodedData = urlEncodedDataPairs.join( '' )
+
+  
+  XHR.addEventListener( 'error', function(event) {
+    alert( 'Oops! Something went wrong.' );
+  } );
+
+  XHR.open( 'POST', 'http://api.kesho.me/v1/user-test/contact' );
+
+
+  XHR.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
+
+  XHR.send( urlEncodedData );
+}
+
+btn.addEventListener( 'click', function() {
+  sendData( {test:'ok'} );
+} )
+sendData()
+}
